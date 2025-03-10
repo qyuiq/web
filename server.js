e6e0fa99f952d7b23864e84dfb40ef81a070b1bc
qyuiq <popow2281337@gmail.com>
2025-03-11 00:58:15 +0700

    вторая лаба

--- server.js
+++ server.js
@@ -1,65 +1,84 @@
-const express = require("express");  
+const express = require("express");
 const path = require("path");
+const cookieParser = require("cookie-parser");
 const fs = require("fs");
 
-const app = express();  
+const app = express();
 const PORT = 3000;
 const PRODUCTS_FILE = path.join(__dirname, "products.json");
 
-// app.use(cors());  
-app.use(express.static(path.join(__dirname, "public")));
-app.use(express.json());
-
-// Функция для чтения товаров из файла
-const readProductsFromFile = () => {
-    if (fs.existsSync(PRODUCTS_FILE)) {
-        const data = fs.readFileSync(PRODUCTS_FILE);
-        return JSON.parse(data);
-    } else {
-        return [];
-    }
+// пользователь
+const USER_DATA = {
+    username: "admin",
+    password: "1234"
 };
 
-// Функция для записи товаров в файл
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+// работа с файлами
+const readProductsFromFile = () => {
+    if (fs.existsSync(PRODUCTS_FILE)) {
+        return JSON.parse(fs.readFileSync(PRODUCTS_FILE));
+    }
+    return [];
+};
+
+// запись в файл
 const writeProductsToFile = (products) => {
     fs.writeFileSync(PRODUCTS_FILE, JSON.stringify(products, null, 2));
 };
 
-// Загружаем товары из файла
-let products = readProductsFromFile();
+// проверяем авторизацию
+app.get("/", (req, res) => {
+    const loggedUser = req.cookies.user;
+    if (!loggedUser || loggedUser !== USER_DATA.username) {
+        return res.render("login", { error: null });
+    }
 
-app.get("/products", (req, res) => {
     const { category, sort } = req.query;
-    let filteredProducts = [...products];
+    let products = readProductsFromFile();
 
     if (category) {
-        filteredProducts = filteredProducts.filter(p => p.category === category);
+        products = products.filter(p => p.category === category);
     }
     if (sort === "asc") {
-        filteredProducts.sort((a, b) => a.price - b.price);
+        products.sort((a, b) => a.price - b.price);
     } else if (sort === "desc") {
-        filteredProducts.sort((a, b) => b.price - a.price);
+        products.sort((a, b) => b.price - a.price);
     }
 
-    res.json(filteredProducts);
+    res.render("index", { products, username: loggedUser });
 });
 
+// вход
+app.post("/login", (req, res) => {
+    const { username, password } = req.body;
+    if (username === USER_DATA.username && password === USER_DATA.password) {
+        res.cookie("user", username, { httpOnly: true});
+        return res.redirect("/");
+    }
+    res.render("login", { error: "Неверные логин или пароль" });
+});
+
+app.get("/logout", (req, res) => {
+    res.clearCookie("user");
+    res.redirect("/");
+});
+
+
+// добавление товара
 app.post("/products", (req, res) => {
     const { name, price, category } = req.body;
-    const newProduct = {
-        id: products.length + 1,
-        name,
-        price: Number(price),
-        category
-    };
+    const products = readProductsFromFile();
+    const newProduct = { id: products.length + 1, name, price: Number(price), category };
     products.push(newProduct);
-
-    // Сохраняем обновленный список товаров в файл
     writeProductsToFile(products);
-
-    res.json(newProduct);
+    res.redirect("/");
 });
 
 app.listen(PORT, () => {
-    console.log(`Сервер запущен http://localhost:${PORT}`);
+    console.log(`Сервер запущен: http://localhost:${PORT}`);
 });
